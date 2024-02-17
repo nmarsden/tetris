e2cc23cc83164ff510b3a9c1b8a746a5ec4864ed
Neil Marsden <neil.marsden@oracle.com>
2024-02-17 15:46:21 +1100

    setup Environment & OrbitControls from @react-three/drei

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import './index.css'
 import {createRoot} from 'react-dom/client'
 import {Canvas} from '@react-three/fiber'
 import {Cube} from "./cube/cube";
+import {Environment, OrbitControls} from "@react-three/drei";
+import {suspend} from 'suspend-react'
+// @ts-ignore
+const warehouse = import('@pmndrs/assets/hdri/warehouse.exr').then((module) => module.default)
 
 const App = () => {
   return (
@@ -12,6 +16,19 @@ const App = () => {
       <ambientLight />
       <pointLight position={[0, 1, -3]} />
       <Cube />
+      { /* @ts-ignore */ }
+      <Environment files={suspend(warehouse)}/>
+      <OrbitControls
+        makeDefault={true}
+        maxPolarAngle={Math.PI / 2}
+        autoRotate={false}
+        autoRotateSpeed={0.25}
+        minDistance={3}
+        maxDistance={10}
+        enableZoom={false}
+        enablePan={false}
+        enableRotate={true}
+      />
     </Canvas>
   )
 }
